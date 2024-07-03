7c044d06ed837b5537d4a94628f2f2fb6820ee20
Davina Taylor <davinataylor@MacBook-Air.local>
2024-07-03 12:52:42 -0700

    made few changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+
+import git
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
